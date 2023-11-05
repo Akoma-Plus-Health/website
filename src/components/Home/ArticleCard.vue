de1be062686cb9e62f96e2dc6fed7c123ec5de0d
tomc <tomc@sindutecsl.com>
2023-11-05 15:10:33 +0000

    updated home

--- src/components/Home/ArticleCard.vue
+++ src/components/Home/ArticleCard.vue
@@ -2,14 +2,18 @@
 import { ref } from 'vue';
 
 const articles = ref([
-  { id: 1, title: "Preventing Heart Disease in Low-Income Communities", desc: "Delve into the critical issue of heart disease prevention in underserved communities and discusses the unique challenges low-income areas face and highlights effective strategies.", },
-  { id: 2, title: "Partnership", desc: "Collaborate with us to expand global heart health initiatives, enhance health insurance offerings, and improve local hospital services." },
-  { id: 3, title: "Workplace", desc: "We partner with businesses and organizations to screen their  workforce for heart disease and stroke risks to promote a healthier and more productive workforce." }
+  {
+    id: 1, title: "Preventing Heart Disease in Low-Income Communities", desc: "Delve into the critical issue of heart disease prevention in underserved communities and discusses the unique challenges low-income areas face and highlights effective strategies."
+  },
+  { id: 2, title: "Heart-Healthy Recipes for a Happy Life", desc: "Discover a collection of delicious recipes that not only tantalize your taste buds but also promote a healthier heart. This features a variety of nutrient-rich and easy-to-make dishes..." },
+  { id: 3, title: "Understanding Cholesterol Levels and It’s Healthy Gains", desc: "This article breaks down the complexities of cholesterol, helping you grasp the different types and their impact on heart health. Gain insights into maintaining optimal cholesterol levels..." }
 ])
 </script>
 <template>
   <!-- article - start -->
-  <div class="flex flex-col overflow-hidden drop-shadow-md bg-white w-[450px]" :key="id" v-for="article in articles">
+  <!-- <div class="flex sm:flex-col overflow-hidden drop-shadow-md bg-white w-[300px] sm:w-[370px]" :key="id"
+    v-for="article in articles"> -->
+  <div class="flex flex-col overflow-hidden rounded-lg border bg-white" :key="id" v-for="article in articles">
     <a href="#" class="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
       <img src="../../assets/images/heart2.png" loading="lazy" alt="Photo by Minh Pham"
         class="absolute inset-0 h-full w-full mix-blend-darken object-cover rounded-lg object-center transition duration-200 group-hover:scale-110" />
