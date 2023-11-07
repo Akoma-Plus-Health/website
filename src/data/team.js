e701f89c430ee39dc8733550e53d94235ceb1801
kristiankf <camoakohene84@gmail.com>
2023-11-07 08:49:30 +0000

    about us

---
+++ src/data/team.js
@@ -0,0 +1,38 @@
+export const team = [
+  {
+    name: 'Dr. Martin Tibuakuu',
+    image: 'martin.png',
+    title: 'Cardiologist & Epidemiologist',
+    linkedin: ''
+  },
+  {
+    name: 'Osei Boateng',
+    image: 'osei.png',
+    title: 'MBA',
+    linkedin: ''
+  },
+  {
+    name: 'Dr. Hamza Asumah',
+    image: 'hamza.png',
+    title: 'General Surgeon',
+    linkedin: ''
+  },
+  {
+    name: 'Dr Bennett Kweku Antwi ',
+    image: 'bennett.png',
+    title: 'MD, MPH, MBA jsc',
+    linkedin: ''
+  },
+  {
+    name: 'Dr. Seth Martin',
+    image: 'seth.png',
+    title: 'Professor of Cardiology',
+    linkedin: ''
+  },
+  {
+    name: 'Dr. Aubrey Mwinyogle ',
+    image: 'aubrey.png',
+    title: 'Vascular Surgery Fellow',
+    linkedin: ''
+  }
+]
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,99 +1,92 @@
+<script setup>
+import AboutHeader from '../components/about/AboutHeader.vue';
+import TeamCard from '../components/about/TeamCard.vue';
+import { team } from '../data/team'
+
+function getImageUrl(name) {
+
+  console.log(new URL(`../assets/images/team/${name}`, import.meta.url).href)
+  return new URL(`../assets/images/team/${name}`, import.meta.url).href
+}
+
+</script>
+
+
 <template>
-  <section class="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48">
-      <!-- image - start -->
-      <img src="@/assets/images/akoma_doctors.png" loading="lazy" alt="Photo by Fakurian Design" class="absolute inset-0 h-full w-full object-cover object-center" />
-      <!-- image - end -->
+  <div>
+    <!-- Header -->
+    <AboutHeader />
 
-      <!-- overlay - start -->
-      <!-- <div class="absolute inset-0 bg-dark-500 mix-blend-multiply"></div> -->
-      <div class="absolute inset-0  backdrop-brightness-50"></div>
-      <!-- overlay - end -->
+    <!-- Mission and Vision -->
+    <section class="container mx-auto my-28">
 
-      <!-- text start -->
-      <div class="relative flex flex-col items-center lg:w-3/4">
-        <h1 class="mb-8 text-center text-4xl font-bold text-white sm:text-6xl md:text-6xl">About Us</h1>
-        <p class="mt-2 text-center text-lg text-white lg:mb-8">At <span class="text-red-600 font-bold">Akoma+</span>, we're dedicated to empowering individuals to take control of their heart health. Our mobile app makes it easy for you to monitor your blood pressure and provides personalized guidance to ensure your heart stays in the best shape possible. Discover a new way to care for your heart with <span class="text-red-600 font-bold">Akoma+</span>.</p>
+      <!-- mission -->
+      <div class="flex justify-center gap-10 items-center">
+        <!-- mission -->
+        <div class="w-[500px]">
+          <!-- mission text -->
+          <div class="bg-white shadow-lg p-8 py-14">
+            <h2 class="text-4xl font-semibold mb-5">Our Mission</h2>
+            <p class="">
+              Using a digital health platform, we seek to provide routine and comprehensive screening for heart disease
+              and stroke risk factors in low- and middle-income countries and provide effective, efficient, and timely
+              prevention strategies.
+            </p>
+          </div>
+        </div>
+
+        <!-- image -->
+        <div class="w-[500px]">
+          <div class="w-[400px] mx-auto">
+            <img src="../assets/images/mission.png" alt="mission" class="w-full">
+          </div>
+        </div>
       </div>
-      <!-- text end -->
-    
+
+      <!-- vision -->
+      <div class="flex justify-center gap-10 flex-row-reverse items-center">
+        <!-- Vision -->
+        <div class="w-[500px]">
+          <!-- Vision text -->
+          <div class="bg-white shadow-lg p-8 py-14">
+            <h2 class="text-4xl font-semibold mb-5">Our Vision</h2>
+            <p class="">
+              To create a world where every individual, regardless of their background or location, has the knowledge,
+              tools, and support needed to achieve and maintain optimal heart health. We envision a future where heart
+              disease is preventable and treatable for all, paving the way for healthier, happier lives.
+            </p>
+          </div>
+        </div>
+
+        <!-- image -->
+        <div class="w-[500px]">
+          <div class="w-[500px] mx-auto">
+            <img src="../assets/images/vision.png" alt="mission" class="w-full">
+          </div>
+        </div>
+      </div>
     </section>
-    <div class="mt-96 absolute bg-gray-300 grid gap-y-10 p-10 rounded-lg sm:grid-cols-2 sm:gap-y-12 lg:grid-cols-3">
-      <!-- quote - start -->
-      <div class="flex flex-col items-center gap-4 sm:px-4 md:gap-6 lg:px-8">
-        <div class="flex flex-col items-center gap-2 md:gap-3">
-          <div class="h-12 w-12 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-14 md:w-14">
-            <img src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=112" loading="lazy" alt="Photo by Radu Florin" class="h-full w-full object-cover object-center" />
-          </div>
 
-          <div class="flex flex-col items-center">
-            <div class="text-center text-sm font-bold text-black sm:text-left md:text-base">1.3 Crore+</div>
-            <p class="text-center text-sm text-gray-500 sm:text-left md:text-sm">Satisfied Patients</p>
-          </div>
-        </div>
+    <!-- Our team -->
+    <section class="container mx-auto my-28">
+      <div class="text-center mb-10">
+        <h2 class="text-4xl font-bold mb-5">Our Team
+        </h2>
+
+        <p class="">
+          We are a team of world-class experts in heart disease prevention and public health.
+        </p>
       </div>
-      <!-- quote - end -->
 
-      <!-- quote - start -->
-      <div class="flex flex-col items-center gap-4 sm:px-4 md:gap-6 lg:px-8">
-              <div class="flex flex-col items-center gap-2 md:gap-3">
-          <div class="h-12 w-12 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-14 md:w-14">
-            <img src="https://images.unsplash.com/photo-1532073150508-0c1df022bdd1?auto=format&q=75&fit=crop&w=112" loading="lazy" alt="Photo by christian ferrer" class="h-full w-full object-cover object-center" />
-          </div>
-
-          <div class="flex flex-col items-center">
-            <div class="text-center text-sm font-bold text-black sm:text-left md:text-base">1.7 Million+</div>
-            <p class="text-center text-sm text-gray-500 sm:text-left md:text-sm">Doctors Appointment</p>
-          </div>
-        </div>
+      <div class="team flex flex-wrap justify-center gap-5">
+        <template v-for="(item, index) in team" :key="index">
+          <TeamCard :name="item.name" :title="item.title" :image="getImageUrl(item.image)" />
+        </template>
       </div>
-      <!-- quote - end -->
-
-      <!-- quote - start -->
-      <div class="flex flex-col items-center gap-4 sm:px-4 md:gap-6 lg:px-8">
-        <div class="flex flex-col items-center gap-2 md:gap-3">
-          <div class="h-12 w-12 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-14 md:w-14">
-            <img src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&q=75&fit=crop&w=500" loading="lazy" alt="Photo by Ayo Ogunseinde" class="h-full w-full object-cover object-center" />
-          </div>
-
-          <div class="flex flex-col items-center">
-            <div class="text-center text-sm font-bold text-black sm:text-left md:text-base">1000+</div>
-            <p class="text-center text-sm text-gray-500 sm:text-left md:text-sm">Certified Experts</p>
-          </div>
-        </div>
-      </div>
-      <!-- quote - end -->
-    </div>
-    <section class="min-h-96 relative flex flex-1 shrink-0 items-center  justify-center overflow-hidden bg-gray-100 py-16 shadow-sm md:py-20 xl:py-48">
-  <div class="w-96 h-48 flex flex-col align-items-center p-4 rounded-lg overflow-hidden border border-red-600">
-    <h3 class="font-bold">Our Mission</h3>
-    <p>Using a digital health platform, we seek to provide routine and comprehensive screening for heart disease and stroke risk factors in low- and middle-income countries and provide effective, efficient, and timely prevention strategies. </p>
-  </div>
-  <div class="w-48 h-48 flex justify-center align-items-center rounded-lg overflow-hidden border border-green-600">
-    <img src="image2.jpg" alt="Image 2" class="w-full h-full object-cover">
-  </div>
-  <div class="w-96 h-48 flex justify-center align-items-center rounded-lg overflow-hidden border border-blue-600">
-    <img src="image3.jpg" alt="Image 3" class="w-full h-full object-cover">
-  </div>
-  <div class="w-96 h-48 flex flex-col p-4 justify-center align-items-center rounded-lg overflow-hidden shadow-lg border border-yellow-600">
-    <h3 class="font-bold">Our Vision</h3>
-    <p>To create a world where every individual, regardless of their background or location, has the knowledge, tools, and support needed to achieve and maintain optimal heart health. We envision a future where heart disease is preventable and treatable for all, paving the way for healthier, happier lives.</p>
-  </div>
-</section>
-
-
-
-    <section class="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48">
-   
     </section>
-    
+
+  </div>
 </template>
 
-<style>
-/* @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-} */
-</style>
+
+<style lang="scss" scoped></style>
