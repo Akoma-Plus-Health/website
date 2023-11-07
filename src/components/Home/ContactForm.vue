4773a2bac94fedcf4e0528808370dc3915be616d
tomc <tomc@sindutecsl.com>
2023-11-07 06:52:38 +0000

    update on contact us page

---
+++ src/components/Home/ContactForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="">
+        <div>
+            <!-- <span class="uppercase text-sm text-gray-600 font-bold">Full Name</span> -->
+            <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="Name*">
+        </div>
+        <div class="mt-4">
+            <!-- <span class="uppercase text-sm text-gray-600 font-bold">Email</span> -->
+            <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="Email">
+        </div>
+        <div class="mt-4">
+            <!-- <span class="uppercase text-sm text-gray-600 font-bold">Email</span> -->
+            <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="Phone Number*">
+        </div>
+        <div class="mt-4">
+            <!-- <span class="uppercase text-sm text-gray-600 font-bold">Message</span> -->
+            <textarea
+                class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                placeholder="How can we help you?"></textarea>
+        </div>
+        <div class="mt-4">
+            <button
+                class="uppercase text-sm font-bold tracking-wide bg-red-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+                Send
+            </button>
+        </div>
+    </div>
+</template>
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,57 +1,53 @@
 <script setup>
+import ContactForm from '../components/Home/ContactForm.vue';
 import Pageheader from '../components/layouts/pageheader.vue';
 
 
 </script>
 <template>
-    <Pageheader />
-    <section>
-<div class=" container w-full mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto text-gray-900">
-  <div class="flex flex-col justify-between">
-    <div>
-      <h2 class="text-4xl lg:text-5xl font-bold leading-tight">Contact <span class="text-red-800">Us</span></h2>
-      <div class="text-gray-700 mt-8">
-        Please contact us with any queries or questions you may have, and we will try our best to respond to you as soon as we can.
+  <Pageheader />
+  <section>
+    <div
+      class=" container w-full mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto text-gray-900">
+      <div class="flex flex-col justify-between">
+        <div>
+          <h2 class="text-4xl lg:text-5xl font-bold leading-tight">Contact <span class="text-red-800">Us</span></h2>
+          <p class="text-gray-700 mt-8">
+            Please contact us with any queries or questions you may have, and we will try our best to respond to you as
+            soon as we can.
+            <!-- If you have any questions or need help, please feel free to contact us! We'll get back to you as soon as
+            possible. -->
+          </p>
+        </div>
+        <div>
+          <img src="@/assets/images/contact-akomaplus.png" alt="get in touch with us" class="drop-shadow-sm">
+        </div>
+        <div class="flex flex-col justify-between">
+          <div>
+            <div class="flex gap-6 text-gray-700 mt-8">
+              <div class="inline-flex gap-3">
+                <img width="23%" src="@/assets/images/icons/phone.png" alt="phone_icon">
+                <span>
+                  <p class="inline-flex gap-1 uppercase font-semibold text-gray-950">
+                    Phone </p>
+                  <p><a href="tel: +14438501407" class="text-red-600">+1-443-850-1407</a></p>
+                </span>
+              </div>
+              <div class="inline-flex gap-3">
+                <img width="23%" src="@/assets/images/icons/email_bag.png" alt="email_bag_icon">
+                <span>
+                  <p class="inline-flex gap-1 uppercase font-semibold text-gray-950"> Email</p>
+                  <p><a href="mailto: info@akomaplushealth.com" class="text-red-600">Info@akomaplushealth.com</a></p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <ContactForm />
     </div>
-  </div>
-  <div class="">
-    <div>
-      <!-- <span class="uppercase text-sm text-gray-600 font-bold">Full Name</span> -->
-      <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-        type="text" placeholder="Name*">
-    </div>
-    <div class="mt-4">
-      <!-- <span class="uppercase text-sm text-gray-600 font-bold">Email</span> -->
-      <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" 
-        type="text" placeholder="Email">
-    </div>
-    <div class="mt-4">
-      <!-- <span class="uppercase text-sm text-gray-600 font-bold">Email</span> -->
-      <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" 
-        type="text" placeholder="Phone Number*">
-    </div>
-    <div class="mt-4">
-      <!-- <span class="uppercase text-sm text-gray-600 font-bold">Message</span> -->
-      <textarea
-        class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" placeholder="How can we help you?"></textarea>
-    </div>
-    <div class="mt-4">
-      <button class="uppercase text-sm font-bold tracking-wide bg-red-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
-        Send
-      </button>
-    </div>
-    <div class="flex flex-col justify-between">
-    <div>
-      <div class="text-gray-700 mt-8">
-        Please contact us with any queries or questions you may have, and we will try our best to respond to you as soon as we can.
-      </div>
-    </div>
-  </div>
-  </div>
-</div>
   </section>
-  </template>
+</template>
   
-  <style>
-  </style>
+<style></style>
