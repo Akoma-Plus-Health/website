63898f73b94b66fea8e2b58adb9f8a00cc260971
tomc <tomc@sindutecsl.com>
2023-11-15 00:43:53 +0000

    new updates

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -85,6 +85,25 @@ function getImageUrl(name) {
       </div>
     </section>
 
+    <section class="py-6 max-w-screen my-32">
+      <div class="text-center mb-10">
+        <h2 class="sub-heading font-bold mb-5">Our Collaborators </h2>
+      </div>
+      <div class="py-10 container mx-auto bg-gray-200">
+        <div class="flex  flex-wrap justify-center gap-4">
+          <div class="w-[200px] h-[200px]\">
+            <img src="@/assets/images/partners/johns_hopkins_medicine_akoma_plus_partner_logo_300_x_300.png" alt="">
+          </div>
+          <div class="w-[200px] h-[200px]">
+            <img src="@/assets/images/partners/google_akoma_plus_partner_logo.png" alt="">
+          </div>
+          <div class="w-[200px] h-[200px">
+            <img src="@/assets/images/partners/fitbit_akoma_plus_partner_logo_340_x_340.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
